0cb8438810f89db0266938fd9df51a3f4c5c55ab
Myllan <Myllan@Juliah.local>
2019-02-27 09:41:20 +0300

    This is to commit that i have create the ui design and also implemented a font to the ui.

---
+++ app/src/main/java/com/example/newsmac/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.newsmac;
+
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.textView) TextView mTextView;
+    @BindView(R.id.getTextView) TextView mGetTextView;
+    @BindView(R.id.sourceTextView) TextView mSourceTextView;
+    @BindView(R.id.topTextView) TextView mTopTextView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        Typeface champagneFont = Typeface.createFromAsset(getAssets(), "fonts/cac_champagne.ttf");
+        mTextView.setTypeface(champagneFont);
+        mGetTextView.setTypeface(champagneFont);
+        mSourceTextView.setTypeface(champagneFont);
+        mTopTextView.setTypeface(champagneFont);
+    }
+}
