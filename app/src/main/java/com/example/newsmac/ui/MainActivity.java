0a76158ad7b2cf27faa22bc36a533f4790290c66
Myllan <Myllan@Juliah.local>
2019-03-14 14:20:17 +0300

    Added all the functions for both the login page and the sign up page and also create the overflow menu

--- app/src/main/java/com/example/newsmac/ui/MainActivity.java
+++ app/src/main/java/com/example/newsmac/ui/MainActivity.java
@@ -4,9 +4,13 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Typeface;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,6 +20,8 @@ import android.widget.Toast;
 import com.example.newsmac.Constants;
 import com.example.newsmac.R;
 import com.example.newsmac.SavedNewsListActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -39,6 +45,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     //private SharedPreferences.Editor mEditor;
     private DatabaseReference mSearchedNewsReference;
     private ValueEventListener mSearchedNewsReferenceListener;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
 
     @Override
@@ -51,6 +59,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 .getInstance()
                 .getReference()
                 .child(Constants.FIREBASE_CHILD_SEARCHED_NEWS);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth){
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user != null) {
+                    getSupportActionBar().setTitle("Welcome," + user.getDisplayName() + "!");
+                } else {
+
+                }
+            }
+        };
 
         mSearchedNewsReferenceListener = mSearchedNewsReference.addValueEventListener(new ValueEventListener() {
             @Override
@@ -120,10 +141,45 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      //   mEditor.putString(Constants.PREFERENCES_LOCATION_KEY, search).apply();
        // }
     @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.action_logout) {
+            logout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
+    }
+    @Override
     protected void onDestroy() {
         super.onDestroy();
 
         mSearchedNewsReference.removeEventListener(mSearchedNewsReferenceListener);
     }
+    @Override
+    public  void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
 
 }
